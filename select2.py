42dc558c653e66710fe56b9a2bbefe36b7948fe4
PavelMetlin <metlin-1@mail.ru>
2022-09-02 10:34:39 +0300

    реализация через assert

--- select2.py
+++ select2.py
@@ -1,3 +1,4 @@
+import unittest
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
@@ -16,20 +17,21 @@ class TestTestselect():
     def test_testselect(self):
         self.driver.get("https://select2.org/selections")
         self.driver.set_window_size(1280, 680)
-        title_text = self.driver.find_element(By.ID, "built-in-escaping")
-        if title_text == self.driver.find_element(By.ID, "built-in-escaping"):
-            print("Мы попали на страницу с текстом - Built-in escaping")
-        else:
-            print("Ошибка элемента")
 
-        sleep(5)
+        assert "built-in-escaping" in self.driver.page_source # вариант через assert
+        #title_text = self.driver.find_element(By.ID, "built-in-escaping")
+        #if title_text == self.driver.find_element(By.ID, "built-in-escaping"):
+            #print("Мы попали на страницу с текстом - Built-in escaping")
+        #else:
+            #print("Ошибка элемента")
+
+
         # первый селект
         self.driver.find_element(By.ID, "body-inner").click()
         self.driver.find_element(By.CSS_SELECTOR, ".select2-selection--single").click()
         self.driver.find_element(By.CSS_SELECTOR, ".select2-search--dropdown > .select2-search__field").click()
         self.driver.find_element(By.CSS_SELECTOR, ".select2-search--dropdown > .select2-search__field").send_keys(
             "California")
-        sleep(3)
         self.driver.find_element(By.CSS_SELECTOR, ".select2-search--dropdown > .select2-search__field").send_keys(
             Keys.ENTER)
         self.driver.save_screenshot('screen1.png')
@@ -39,7 +41,6 @@ class TestTestselect():
         self.driver.find_element(By.CSS_SELECTOR, ".select2-selection--multiple").click()
         self.driver.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys("Hawaii")
         self.driver.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys(Keys.ENTER)
-        sleep(3)
         self.driver.save_screenshot('screen2.png')
         self.driver.find_element(By.LINK_TEXT, "internal representation of the selected option").click()
         self.driver.save_screenshot('screen3.png')
